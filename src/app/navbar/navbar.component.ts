93124355b0a9cd9b7b1f24cab2b0c1726e01b092
lahcen404 <lahcen.maskour2003@gmail.com>
2025-04-25 10:19:56 +0100

    Display Menu in Navbar by Role (Authenticated or Anonymous)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../core/guards/auth.service';
+import { UserInterface } from '../user.interface';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
-  isMenuVisible = false;
+export class NavbarComponent implements OnInit {
+  isMenuOpen = false;
+  currentUser = signal<UserInterface | null>(null);
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit() {
+    // Sync the currentUser signal with the AuthService's currentUserSig
+    this.currentUser = this.authService.currentUserSig;
+  }
 
   toggleMenu() {
-    this.isMenuVisible = !this.isMenuVisible;
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  signOut() {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err: any) => {
+        console.error('Logout failed:', err);
+      }
+    });
   }
 }
